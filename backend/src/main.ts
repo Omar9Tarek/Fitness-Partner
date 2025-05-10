de370e24f9fb83a5bde50d4e6bed2124c6e3e183
Omar9Tarek <Omar9Tarek@gmail.com>
2025-05-10 12:27:49 +0300

    fixations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
   // CORS configuration
   app.enableCors({
-    origin: 'https://fitness-partner-no76.vercel.app' ,
+    origin: 'https://fitness-partner-back.vercel.app' ,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
